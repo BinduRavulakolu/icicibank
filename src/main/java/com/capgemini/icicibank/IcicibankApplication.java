4364c88ee8972e083360634b71e533b1c1259276
bravulak <bravulak@IN-MUM-LDVM005.corp.capgemini.com>
2018-09-27 03:29:08 -0400

    second commit

--- src/main/java/com/capgemini/icicibank/IcicibankApplication.java
+++ src/main/java/com/capgemini/icicibank/IcicibankApplication.java
@@ -1,9 +1,11 @@
 package com.capgemini.icicibank;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = { "com.capgemini.icicibank" })
+//@EnableAutoConfiguration
 public class IcicibankApplication {
 
 	public static void main(String[] args) {
